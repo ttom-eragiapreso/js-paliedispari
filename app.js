46edd3129af35898ec7b379a298012c9f2386909
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-10-19 14:44:22 +0200

    Palindromo Finito

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log("App loaded")
+
+// *Palidroma**
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// **Pari e Dispari**
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+
+
+// PALINDROMA
+
+// let userWord = prompt("Scrivi una parola");
+
+function isPalindroma(word){
+
+  let index = word.length -1;
+  let reverseWord = [];
+  while(index >= 0){
+    reverseWord.push(word[index])
+    index--
+  }
+
+  reverseWord = reverseWord.join("");
+  console.log(reverseWord);
+
+  if(reverseWord === word){
+    return true
+  }
+  return false;
+}
+
+console.log(isPalindroma("ossesso"));
